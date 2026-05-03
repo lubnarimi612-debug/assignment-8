90a14b018e2d42b4391316e87690f8f959a4fdf3
evrahimhasan <evrahimhasanemran.com>
2026-05-03 19:01:22 +0600

    mongodb update

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("tiles");
+const db = client.db("bookstore");
 
 export const auth = betterAuth({
   emailAndPassword: {
