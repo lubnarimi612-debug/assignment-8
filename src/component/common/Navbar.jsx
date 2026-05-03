2846681e2aed74e26c487ba5d960b51286fff5a8
evrahimhasan <evrahimhasanemran.com>
2026-05-03 18:32:44 +0600

    sign in sign up add

--- src/component/common/Navbar.jsx
+++ src/component/common/Navbar.jsx
@@ -2,8 +2,30 @@
 import React from 'react';
 import { BookOpen, Home, Library, User, ChevronDown } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import { authClient } from '@/lib/auth-client';
+import { toast } from 'react-toastify';
+import Image from 'next/image';
 
 const Navbar = () => {
+  const userData = authClient.useSession();
+  const user = userData?.data?.user;
+  // console.log('User Data:', user);
+  const pathname = usePathname();
+  // console.log('Current Path:', pathname); 
+  const router = useRouter();
+  const handleLogOut = async () => {
+    const { data, error } = await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/"); // redirect to login page
+        },
+      },
+    });
+    if (!error) {
+      toast.success("You have been logged out successfully");
+    }
+  };
   
   return (
     <div className="navbar bg-base-300 mx-auto shadow-sm px-4 md:px-8 border-b border-gray-100">
@@ -24,17 +46,20 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 gap-4 font-medium">
           <li className="">
-            <Link href="/" className="flex items-center gap-1.5 py-4 hover:text-green-800">
+            <Link href="/" className={`flex items-center gap-1.5 pt-4 hover:text-green-800 ${
+  pathname === "/" ? "border-b-2   rounded-none border-green-800" : ""}`}>
               <Home size={18} /> Home
             </Link>
           </li>
           <li>
-            <Link href="/allbooks" className="flex items-center gap-1.5 py-4  hover:text-green-800">
+            <Link href={`/allbooks`} className={`flex items-center gap-1.5 pt-4  hover:text-green-800 ${
+  pathname === "/allbooks" ? "border-b-2   rounded-none border-green-800" : ""}`}>
               <Library size={18} /> All Books
             </Link>
           </li>
           <li>
-            <Link href="/profile" className="flex items-center gap-1.5 py-4  hover:text-green-800">
+            <Link href="/profile" className={`flex items-center gap-1.5 pt-4  hover:text-green-800 ${
+  pathname === "/profile" ? "border-b-2   rounded-none border-green-800" : ""}`}>
               <User size={18} /> My Profile
             </Link>
           </li>
@@ -42,25 +67,35 @@ const Navbar = () => {
       </div>
 
       {/* Profile & Logout Section */}
-      <div className="navbar-end gap-4">
+      {
+        user ? <div className="navbar-end gap-4">
         {/* User Info */}
         <div className="flex items-center gap-2 cursor-pointer group">
           <div className="avatar">
             <div className="w-10 rounded-full ring ring-offset-2 ring-gray-100">
-              <img src="https://via.placeholder.com/150" alt="User Profile" />
+              <Image width={1000} height={1000} src={user.image} referrerPolicy="no-referrer" alt="User Profile" />
             </div>
           </div>
           <div className="hidden sm:flex items-center gap-1">
-            <span className="text-sm font-semibold text-gray-700">Tanvir Ahmed</span>
+            <span className="text-sm font-semibold text-gray-700"> {user.name}</span>
             <ChevronDown size={16} className="text-gray-500" />
           </div>
         </div>
 
         {/* Logout Button (DaisyUI style) */}
-        <button className="btn btn-sm md:btn-md bg-green-800 hover:bg-green-900 text-white border-none px-6">
+        <button onClick={handleLogOut} className="btn btn-sm md:btn-md bg-green-800 hover:bg-green-900 text-white border-none px-6">
           Logout
         </button>
+      </div> : <div className="navbar-end">
+        <Link href="/login" className="btn btn-sm md:btn-md bg-green-800 hover:bg-green-900 text-white border-none px-6">
+          Login
+        </Link>
+        <Link href="/signup" className="btn ml-4 btn-sm md:btn-md bg-green-800 hover:bg-green-900 text-white border-none px-6">
+          Sign Up
+        </Link>
       </div>
+      }
+      
     </div>
   );
 };
